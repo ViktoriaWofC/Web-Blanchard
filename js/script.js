8d24794dcc92f4f14b7b6204ae3065ed73ee5cf1
ViktoriaWofC <vik-tori-s@yandex.ru>
2021-04-02 21:54:58 +0400

    complete contacts

--- js/script.js
+++ js/script.js
@@ -135,6 +135,34 @@ window.addEventListener('DOMContentLoaded', function() {
   });
 
 
+  // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  ymaps.ready(init);
+  function init(){
+      // Создание карты.
+      var myMap = new ymaps.Map("map", {
+          // Координаты центра карты.
+          // Порядок по умолчанию: «широта, долгота».
+          // Чтобы не определять координаты центра карты вручную,
+          // воспользуйтесь инструментом Определение координат.
+          center: [55.763463,37.611079],
+          // Уровень масштабирования. Допустимые значения:
+          // от 0 (весь мир) до 19.
+          zoom: 14,
+          controls: ['geolocationControl', 'zoomControl']
+      });
+
+      myMap.controls.get('zoomControl').options.set('size', 'small');
+
+      var myPlacemark = new ymaps.Placemark([55.758463,37.601079], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '/img/map_point.png',
+        iconImageSize: [20, 20]
+      });
+      myMap.geoObjects.add(myPlacemark);
+  }
+
+
   // document.querySelectorAll(".ui-accordion-content").forEach(function(el){
   //   el.classList.remove("ui-accordion-content-active");
   // });
