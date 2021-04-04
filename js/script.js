eec6cad85d482e438647050f0ff8301f2543f287
ViktoriaWofC <vik-tori-s@yandex.ru>
2021-04-04 15:07:31 +0400

    start all hovers

--- js/script.js
+++ js/script.js
@@ -55,8 +55,6 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-
-
   document.querySelectorAll(".country-list-item-link").forEach(function(elem) {
     elem.addEventListener("click", function(event){
       ChangeCountyTabAction(event);
@@ -195,30 +193,30 @@ window.addEventListener('DOMContentLoaded', function() {
 
   // Функция ymaps.ready() будет вызвана, когда
   // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-  ymaps.ready(init);
-  function init(){
-      // Создание карты.
-      var myMap = new ymaps.Map("map", {
-          // Координаты центра карты.
-          // Порядок по умолчанию: «широта, долгота».
-          // Чтобы не определять координаты центра карты вручную,
-          // воспользуйтесь инструментом Определение координат.
-          center: [55.763463,37.611079],
-          // Уровень масштабирования. Допустимые значения:
-          // от 0 (весь мир) до 19.
-          zoom: 14,
-          controls: ['geolocationControl', 'zoomControl']
-      });
+  // ymaps.ready(init);
+  // function init(){
+  //     // Создание карты.
+  //     var myMap = new ymaps.Map("map", {
+  //         // Координаты центра карты.
+  //         // Порядок по умолчанию: «широта, долгота».
+  //         // Чтобы не определять координаты центра карты вручную,
+  //         // воспользуйтесь инструментом Определение координат.
+  //         center: [55.763463,37.611079],
+  //         // Уровень масштабирования. Допустимые значения:
+  //         // от 0 (весь мир) до 19.
+  //         zoom: 14,
+  //         controls: ['geolocationControl', 'zoomControl']
+  //     });
 
-      myMap.controls.get('zoomControl').options.set('size', 'small');
+  //     myMap.controls.get('zoomControl').options.set('size', 'small');
 
-      var myPlacemark = new ymaps.Placemark([55.758463,37.601079], {}, {
-        iconLayout: 'default#image',
-        iconImageHref: '/img/map_point.png',
-        iconImageSize: [20, 20]
-      });
-      myMap.geoObjects.add(myPlacemark);
-  }
+  //     var myPlacemark = new ymaps.Placemark([55.758463,37.601079], {}, {
+  //       iconLayout: 'default#image',
+  //       iconImageHref: '/img/map_point.png',
+  //       iconImageSize: [20, 20]
+  //     });
+  //     myMap.geoObjects.add(myPlacemark);
+  // }
 
 
 
